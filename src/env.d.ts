0d23813a40fd548e7a3f1d63050e64c080b1d7c2
Alfredo <dytalfredomendoza@gmail.com>
2024-08-06 18:50:51 -0400

    yipe

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+/// <reference types="astro/server" />
 
 interface ImportMetaEnv {
 	readonly API_URL: string
